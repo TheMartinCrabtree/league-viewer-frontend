456ea0bc170214c8b452bebd66b5ea418f56b2bf
MC-Games\MC <martin.crabtree@gmail.com>
2020-01-30 15:18:47 -0500

    more formatting

--- src/components/ChampList.js
+++ src/components/ChampList.js
@@ -5,6 +5,9 @@ const ChampList=(props)=>{
     return(
         <section>
             <h3>This is the Champ List</h3>
+            <p>filter bar will go here</p>
+            <p>list will go here</p>
+            <p>details panel will go here</p>
         </section>
     );
 
